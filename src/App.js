ac42309292114ec8c96e12f83ac2a0948879c7a7
Ivan Krovikov <ivankrovikov78@gmail.com>
2021-02-18 17:00:55 +0200

    Add Counter

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Counter from './components/Counter';
 
 function App(props) {
-  const step = 10;
 
-  return <Counter step={step} />;
+
+  return (
+    <>
+      <Counter name='Counter' step={10} />
+    </>
+
+
+  );
 }
 
 export default App;
--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-
-class Counter extends Component {
+import styles from './Counter.module.css';
+import PropTypes from 'prop-types';
+ class Counter extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       count: 0,
       isAdd: true,
@@ -12,42 +12,42 @@ class Counter extends Component {
 
   handleClick = () => {
     this.setState((state, props) => {
-      if (state.isAdd) {
-        const newCount = state.count + props.step;
-        return { 
-          count: newCount,
-        };
-      } else {
-        const newCount = state.count + props.step;
-        return { 
-          count: newCount,
-        };
-      }
+      return { 
+        count: state.isAdd
+        ? state.count + props.step
+        : state.count - props.step,
+      };
     });
   };
 
   changeMode = () =>{
     this.setState({ 
       isAdd: !this.state.isAdd,
-    
     });
-  }
+  };
 
   render () {
-    const { count } = this.state;
+    const { count, isAdd } = this.state;
     const { step } = this.props;
-
     return (
-      <div>
-        <div>Текущий Счёт: {count}</div>
-        <div>Шаг: {step}</div>
-        <button onClick={this.handleClick}>
-          {isAdd ? 'ДОБАВИТЬ' : 'ОТНЯТЬ'}
-        </button>
-        <button onClick={this.changeMode}>Изменить Режим</button>
-      </div>
+      <article className={styles.container}>
+        <h1 className={styles.count}>Текущий счет {count}</h1>
+        <div className={styles.count}> Шаг вычисления {step}</div>
+        <div>
+          <button className={styles.btn} onClick={this.handleClick}>
+            {' '}
+            {isAdd ? 'Добавление' : 'Вычитание'}
+          </button>
+          <button className={styles.btn} onClick={this.changeMode}>
+            Режим 
+          </button>
+        </div>
+      </article>
     );
   }
+}
+Counter.propTypes = {
+  step: PropTypes.number.isRequired,
 }
 
 export default Counter;
